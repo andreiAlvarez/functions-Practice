89f8214988727f393021bd3c3eae224a70954f03
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-11-14 16:43:32 -0500

    Update function.js

--- function.js
+++ function.js
@@ -13,3 +13,7 @@ const formatMoney = amount => `$${amount.toFixed(2)}`;
 // function 4
 
 const product = (a, b) => b - a > 1 ? Array.from(Array(b - a - 1), (_, i) => a + ++i).reduce((r, e) => r * e) :'Error';
+
+// function 5
+
+const findNeedle =haystack => "found the needle at position " + haystack.indexOf("needle");
