2901f777184fc5653181639a69a9b49d5f76bc11
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-11-13 19:46:43 -0500

    Update function.js

--- function.js
+++ function.js
@@ -9,3 +9,7 @@ const unusualFive = () => ['a', 'b', 'c', 'd', 'e'].length;
 // function 3
 
 const formatMoney = amount => `$${amount.toFixed(2)}`; 
+
+// function 4
+
+const product = (a, b) => b - a > 1 ? Array.from(Array(b - a - 1), (_, i) => a + ++i).reduce((r, e) => r * e) :'Error';
