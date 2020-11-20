1abb9006e5384b38f7c24ca74cd391b124658cae
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-11-19 23:57:33 -0500

    Update function.js

--- function.js
+++ function.js
@@ -25,3 +25,26 @@ const datingRange = (age) => age>=14?`${Math.floor(age/2+7)}-${Math.floor((age-7
 // kata 7 
 
 const DNAtoRNA = dna => dna.replace(/T/g, 'U');
+
+// kata 8
+
+// Add the Houses to the array
+var houses = [  
+  {name: "Targaryen", motto: "Fire and Blood"},
+  {name: "Stark",     motto: "Winter is Coming"},
+  {name: "Bolton",    motto: "Our Blades Are Sharp"},
+  {name: "Greyjoy",   motto: "We Do Not Sow"},
+  {name: "Tully",     motto: "Family, Duty, Honor"},
+  {name: "Arryn",     motto: "As High as Honor"},
+  {name: "Lannister", motto: "Hear Me Roar!"},
+  {name: "Tyrell",    motto: "Growing Strong"},
+  {name: "Baratheon", motto: "Ours is the Fury"},
+  {name: "Martell",   motto: "Unbowed, Unbent, Unbroken"}
+];
+var targaryen = {name: "Targaryen", motto: "Fire and Blood"};
+
+// Implement the function
+function motto(name) {
+   const search = houses.find(v=>v.name===name)
+   return search?search.motto:''
+}
